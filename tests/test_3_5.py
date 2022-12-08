68d423413dcc77b97db1e5673b84d9d6d669d388
Asel <bis-aselya@yandex.ru>
2022-12-08 23:47:48 +0400

    Update test_3_5.py

--- tests/test_3_5.py
+++ tests/test_3_5.py
@@ -3,23 +3,26 @@ from selene import have
 from selene.support.shared import browser
 
 
-def test_form():
+def test_submit_form():
     browser.element('#firstName').type('Asel')
     browser.element('#lastName').type('Bisengalieva')
     browser.element('#userEmail').type('test@email.com')
-    browser.element('[for="gender-radio-2"]').click()
+    browser.element('[name=gender][value=Female]+label').click()
     browser.element('#userNumber').type('1234567890')
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').type("June")
     browser.element('.react-datepicker__year-select').type("1995")
-    browser.element('[aria-label="Choose Saturday, June 24th, 1995"]').click()
+    browser.element('[aria-label=Choose Saturday, June 24th, 1995]').click()
     browser.element('#subjectsInput').type('Computer Science').press_enter()
-    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('[for=hobbies-checkbox-2]').click()
+    
     browser.element('#uploadPicture').set_value(
         os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'files/foto.jpg')))
     browser.element('#currentAddress').type('Saratov')
+    
     browser.element('#react-select-3-input').type('Haryana').press_enter()
     browser.element('#react-select-4-input').type('Panipat').press_enter()
+    
     browser.element('#submit').press_enter()
 
     browser.all('.table-responsive td:nth-child(2)').should(have.texts(
