c82c52f2ad3a8d29c9bd89446f3b80a89f97b2ae
aselka <bis-aselya@yandex.ru>
2022-12-14 19:01:19 +0400

    new commit

--- tests/test_3_5.py
+++ tests/test_3_5.py
@@ -18,21 +18,21 @@ def test_submit_form():
     
     browser.element('#uploadPicture').set_value(
         os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'files/foto.jpg')))
-    browser.element('#currentAddress').type('Saratov')
+    browser.element('#currentAddress').type('Test Street 12')
     
     browser.element('#react-select-3-input').type('Haryana').press_enter()
     browser.element('#react-select-4-input').type('Panipat').press_enter()
     
     browser.element('#submit').press_enter()
 
-    browser.element('.table').all('td').even.should(have.texts(
-         'Asel Bisengalieva',
-         'test@email.com',
-         'Female',
-         '1234567890',
-         '24 June,1995',
-         'Computer Science',
-         'Reading',
-         'foto.jpg',
-         'Saratov',
-         'Haryana Panipat'))
+    browser.element('.table').all('td').should(have.texts(
+        ('Student Name', 'Asel Bisengalieva'),
+        ('Student Email', 'test@email.com'),
+        ('Gender', 'Female'),
+        ('Mobile', '1234567890'),
+        ('Date of Birth', '24 June,1995'),
+        ('Subjects', 'Computer Science'),
+        ('Hobbies', 'Reading'),
+        ('Picture', 'foto.jpg'),
+        ('Address', 'Saratov'),
+        ('State and City', 'Haryana Panipat')))
